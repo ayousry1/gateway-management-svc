eaa1a175e7cd070ddc3d707cdf86916310d760d6
ayousry <ayousry@sumerge.com>
2021-09-10 13:19:09 +0200

    [main]: removing useless utils

--- src/main/java/com/magement/gateway/common/Utilities.java
+++ src/main/java/com/magement/gateway/common/Utilities.java
@@ -1,28 +1,9 @@
 package com.magement.gateway.common;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.uuid.Generators;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 
-import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
-import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
-
 public final class Utilities {
-
-    public static String generateUUID() {
-        return Generators.timeBasedGenerator().generate().toString();
-    }
-
-    public static ObjectMapper getObjectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
-        mapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return mapper;
-    }
-
     public static ModelMapper getModelMapper() {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
